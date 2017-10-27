010f9112036e5185ba954b70927d52562f8bd16e
gardethk <jose.garcia.14@sansano.usm.cl>
2017-10-26 23:13:36 -0300

    Agregados threads al servidor
    Se agregaron los threads. Estos se crean cuando el socket de bienvenida recibe una conexion. El metodo run() es lo que el thread hace. Hay que seguir trabajando en run().

--- ServidorCentral.java
+++ ServidorCentral.java
@@ -2,26 +2,46 @@ package com.company;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
+
+class socketThread extends Thread {
+    private Socket socketCliente;
+    socketThread(Socket sock) {
+        socketCliente=sock;
+    }
+    public void run() {
+        try {
+            BufferedReader mensajeRecibido = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+            DataOutputStream respuesta = new DataOutputStream(socketCliente.getOutputStream());
+            String request = mensajeRecibido.readLine();
+            String IPCliente = socketCliente.getLocalAddress().toString();
+            System.out.println("Dar autorizacion a " + IPCliente + " ? (y/n)");
+            BufferedReader inputConsola = new BufferedReader(new InputStreamReader(System.in));
+            String autorizacion = inputConsola.readLine();
+            if(Objects.equals(autorizacion,"y")){
+                //COSAS HERMOSAS
+            }
+            else socketCliente.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 class ServidorCentral {
+
     public static void main(String args[]) throws Exception {
         int firsttime = 1;
         while (true) {
             ServerSocket socketDeBienvenida = new ServerSocket(4000);
             System.out.println("running... on port: 4000");
-            Socket socketCliente = socketDeBienvenida.accept();
-            BufferedReader mensajeRecibido = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-            DataOutputStream respuesta = new DataOutputStream(socketCliente.getOutputStream());
-            String request = mensajeRecibido.readLine();
-            String IPCliente = socketCliente.getLocalAddress().toString();
-            System.out.println("Dar autorizacion a "+ IPCliente +" ? (y/n)");
-            
+            socketThread thread = new socketThread(socketDeBienvenida.accept());
+            thread.start();
             socketDeBienvenida.close();
-            System.out.println("connection terminated");
         }
     }
 }
