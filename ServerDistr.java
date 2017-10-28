00dae52f6ea137883dbc07f42912955e93059bd8
gardethk <jose.garcia.14@sansano.usm.cl>
2017-10-28 08:54:33 -0300

    Actualizacion
    Viendo que aqui es donde hay mas clases (xd) agregue cosas a las clases Titan y ServerDistr, mas que cosas logicas, agregue las cosas paja, onda lo que no ocupa sockets.

--- ServerDistr.java
+++ ServerDistr.java
@@ -1,2 +1,35 @@
+package com.company;
+
+import java.util.List;
+
 public class ServerDistr {
+    private Distrito distrito;
+    private List<Titan> titanes;
+    private List<Titan> capturados;
+    private List<Titan> asesinados;
+
+    public void captura(int ID) {
+        for (Titan i : titanes) {
+            if (i.getId() == ID && (i.tipoNormal() || i.tipoCambiante())) {
+                i.setUltimoDistrito(distrito);
+                capturados.add(i);
+                titanes.remove(i);
+            }
+        }
+    }
+
+    public void muerte(int ID) {
+        for (Titan i : titanes) {
+            if (i.getId() == ID && (i.tipoNormal() || i.tipoExcentrico())) {
+                i.setUltimoDistrito(distrito);
+                asesinados.add(i);
+                titanes.remove(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //codigo
+        //Hay que poner la llamada a cada uno de los metodos en base al mensaje que se reciba
+    }
 }
